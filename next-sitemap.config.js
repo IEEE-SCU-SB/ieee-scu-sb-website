d9e54ddfd898ecb08f7e05cd471df89f791758f0
Mostafa <mostafaahmed099@gmail.com>
2025-04-19 00:01:46 +0200

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://ieeescu.vercel.app',
+    siteUrl: 'https://ieeescu.org',
     generateRobotsTxt: true,
   }
   
