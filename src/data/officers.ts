78379698bfdebbce87a016e928643122e31150d0
saeedinum <mo7amed0ptr@gmail.com>
2025-09-29 01:03:13 +0300

    new highboard data

--- src/data/officers.ts
+++ src/data/officers.ts
@@ -1,52 +1,52 @@
-import { Officer } from "./types";
-import ethar from "@/assets/highboard/ethar.webp";
-import abdelkhalek from "@/assets/highboard/abdelkhalek.webp";
-import marwan from "@/assets/highboard/marwan.webp";
-import singab from "@/assets/highboard/singab.webp";
-import ashrakat from "@/assets/highboard/ashrakat.webp";
-import mostafa from "@/assets/highboard/mostafa.webp";
+import {Officer} from "./types";
+import chair from "@/assets/highboard/chair.webp";
+import treasurer from "@/assets/highboard/treasurer.webp";
+import vice2 from "@/assets/highboard/vice2.webp";
+import vice1 from "@/assets/highboard/vice1.webp";
+import webmaster from "@/assets/highboard/webmaster.webp";
+import secretary from "@/assets/highboard/secretary.webp";
 
 export const officers: Officer[] = [
-  {
-    name: "Marwan Ashraf",
-    position: "Vice Chair",
-    image: { src: marwan, alt: "Vice Chair" },
-    linkedin: "https://www.linkedin.com/in/marwan-ashraf-ibrahim",
-    mail: "marwanelgammal0@gmail.com",
-  },
-  {
-    name: "Ethar Hassan",
-    position: "Chairwoman",
-    image: { src: ethar, alt: "Chair" },
-    linkedin: "https://www.linkedin.com/in/etharhassan22",
-    mail: "etharhassan126@gmail.com",
-  },
-  {
-    name: "Ahmed Singab",
-    position: "Vice Chair",
-    image: { src: singab, alt: "Vice Chair" },
-    linkedin: "https://www.linkedin.com/in/ahmed-singab-69a692272",
-    mail: "ahmedsingab06@gmail.com",
-  },
-  {
-    name: "Ashrakat Rashwan",
-    position: "Treasurer",
-    image: { src: ashrakat, alt: "Treasurer" },
-    linkedin: "https://www.linkedin.com/in/ashrakat-rashwan",
-    mail: "ashrakat_rashwan@ieee.org",
-  },
-  {
-    name: "M. Abdelkhalek",
-    position: "Secretary",
-    image: { src: abdelkhalek, alt: "Secretary" },
-    linkedin: "https://www.linkedin.com/in/muhammed-abdelkhalek-33055420a",
-    mail: "mohamma2002@yahoo.com",
-  },
-  {
-    name: "Mostafa Hashem",
-    position: "Webmaster",
-    image: { src: mostafa, alt: "Webmaster" },
-    linkedin: "https://www.linkedin.com/in/mostafaaa-hashem",
-    mail: "mostafahashem.fe@gmail.com",
-  },
+	{
+		name: "Ahmed Singab",
+		position: "Vice Chair",
+		image: {src: vice1, alt: "Vice Chair"},
+		linkedin: "https://www.linkedin.com/in/ahmed-singab-69a692272",
+		mail: "ahmedsingab06@gmail.com",
+	},
+	{
+		name: "Ashrakat Rashwan",
+		position: "Chairwoman",
+		image: {src: chair, alt: "Chair"},
+		linkedin: "https://www.linkedin.com/in/ashrakat-rashwan",
+		mail: "ashrakat_rashwan@ieee.org",
+	},
+	{
+		name: "Yara Ali",
+		position: "Vice Chair",
+		image: {src: vice2, alt: "Vice Chair"},
+		linkedin: "https://www.linkedin.com/in/yara-ali-821061344?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+		mail: "yaraalimaati2@gmail.com",
+	},
+	{
+		name: "Arwa Ahmed",
+		position: "Treasurer",
+		image: {src: treasurer, alt: "Treasurer"},
+		linkedin: "https://www.linkedin.com/in/arwa-ahmed-55a343367",
+		mail: "arwaahmeddsoliman28@gmail.com",
+	},
+	{
+		name: "Mariam Abdou",
+		position: "Secretary",
+		image: {src: secretary, alt: "Secretary"},
+		linkedin: "https://www.linkedin.com/in/mariam-abdou-ibrahim",
+		mail: "mariamabddoo@gmail.com",
+	},
+	{
+		name: "Mohamed Alsaeed",
+		position: "Webmaster",
+		image: {src: webmaster, alt: "Webmaster"},
+		linkedin: "https://www.linkedin.com/in/saeedinum",
+		mail: "saeedinum@gmail.com",
+	},
 ];
