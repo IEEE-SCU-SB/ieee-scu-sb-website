be0e847fd98bf94bccfbaff01572b5b55920ca7b
saeedinum <mo7amed0ptr@gmail.com>
2025-11-14 14:05:49 +0200

    refactor: expand the usage of cloudinary to store board and events images

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,22 +1,6 @@
 import {LatestEvent, UpcomingEvent} from "./types";
-// upcoming Events Images
-import techne from "@/assets/events/upcoming/techne.webp";
-
-// Latest Events Images
-import robotiva from "@/assets/events/latest/robotiva.webp";
-import antiTheHack from "@/assets/events/latest/anti_the_hack.webp";
-import codingHive from "@/assets/events/latest/coding_hive.webp";
-import promptEng from "@/assets/events/latest/prompt_eng.webp";
-import huaweiIct from "@/assets/events/latest/huawei_ict.webp";
-import uiuxEssentials from "@/assets/events/latest/uiux_essentials.webp";
-import hrGuided from "@/assets/events/latest/hr_guided.webp";
-import makeYourReel from "@/assets/events/latest/make_your_reel.webp";
-
-// Note: Events are in reverse chronological order with the most recent event being first in the data structure.
 
 export const upcomingEvents: UpcomingEvent[] = [];
-
-/* ------------------------------------------------------------------------------------ */
 
 export const latestEvents: LatestEvent[] = [
 	{
@@ -26,7 +10,7 @@ export const latestEvents: LatestEvent[] = [
 		location: "Creativa Hub, Ismailia",
 		description: `Robotiva, organized by IEEE in collaboration with Creativa Hub, Ismailia, brought together aspiring engineers and tech enthusiasts to design, build, and compete with their own robots.
     The event offered a high-energy, challenge-driven environment where participants showcased creativity, teamwork, and problem-solving skills in real-world robotics scenarios.`,
-		image: {src: robotiva, alt: "Robotiva Competition"},
+		image: {src: "https://res.cloudinary.com/ieeescusb/image/upload/v1763109238/robotiva_asq0xv.webp", alt: "Robotiva Competition"},
 		socialMediaPostLinks: {
 			facebook: "https://www.facebook.com/share/r/1AXQdMwQo3",
 			instagram: "https://www.instagram.com/reel/DGncONGMjJ-/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
@@ -38,7 +22,7 @@ export const latestEvents: LatestEvent[] = [
 		date: "Feb 26, 2025",
 		location: "Faculty of Engineering, SCU",
 		description: `Our Cybersecurity Session, led by Eng. Mohamed Fareed — Head of Cybersecurity, offered participants a solid and accessible introduction to digital security. The session covered essential topics such as malware, social engineering, and major cybersecurity threats, while also providing practical guidance on staying protected in today's connected world.`,
-		image: {src: antiTheHack, alt: "Anti The Hack Session"},
+		image: {src: "https://res.cloudinary.com/ieeescusb/image/upload/v1763109235/anti_the_hack_loi2pu.webp", alt: "Anti The Hack Session"},
 		socialMediaPostLinks: {
 			facebook: "https://www.facebook.com/share/r/1DNcEHFW6R",
 			instagram: "https://www.instagram.com/reel/DGi3u-1M8yK/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
@@ -51,7 +35,7 @@ export const latestEvents: LatestEvent[] = [
 		location: "ITI, Ismailia",
 		description: `The Coding Hive Hackathon, organized by IEEE in collaboration with MarketAix Agency, introduced participants to the world of programming from scratch—literally! 
     Using Scratch, participants worked in teams to design and build creative projects, learning core programming concepts step by step. With interactive challenges and a special prize, the event made learning to code both exciting and accessible.`,
-		image: {src: codingHive, alt: "Coding Hive Hackathon"},
+		image: {src: "https://res.cloudinary.com/ieeescusb/image/upload/v1763109236/coding_hive_nhh1hu.webp", alt: "Coding Hive Hackathon"},
 		socialMediaPostLinks: {
 			facebook: "https://www.facebook.com/share/p/1KVtyA8E8N",
 			instagram: "https://www.instagram.com/p/DGdgboyMs8B/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
@@ -66,7 +50,7 @@ export const latestEvents: LatestEvent[] = [
 		location: "ITI, Ismailia",
 		description: `From designing stunning videos and generating original songs to building responsive websites and crafting logos, this session showcased the power of AI as a creative tool. 
       Participants explored a wide range of AI-driven applications and left inspired by how these tools can enhance creativity across nearly every field.`,
-		image: {src: promptEng, alt: "Prompt Engineering Workshop"},
+		image: {src: "https://res.cloudinary.com/ieeescusb/image/upload/v1763109238/prompt_eng_pqik0f.webp", alt: "Prompt Engineering Workshop"},
 		socialMediaPostLinks: {
 			facebook: "https://www.facebook.com/100064157166643/posts/954951156653448/?mibextid=rS40aB7S9Ucbxw6v",
 			instagram: "https://www.instagram.com/p/DDNIKXjMRjv/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
@@ -82,7 +66,7 @@ export const latestEvents: LatestEvent[] = [
 		description:
 			"The Huawei Global ICT Competition 2024-2025 launch was successfully held at the Faculty of Engineering, Suez Canal University. The event featured insightful discussions and gave students the opportunity to explore advanced technologies in AI, Cloud Computing, Communications, and Networks, fostering innovation and technical expertise.",
 		image: {
-			src: huaweiIct,
+			src: "https://res.cloudinary.com/ieeescusb/image/upload/v1763109237/huawei_ict_tpioa1.webp",
 			alt: "Huawei Global ICT Competition for 2024-2025 Launch",
 		},
 		socialMediaPostLinks: {
@@ -99,7 +83,7 @@ export const latestEvents: LatestEvent[] = [
 		location: "Creativa Hub, Ismailia",
 		description:
 			"Creativity met functionality in this hands-on UI/UX design session. Participants explored the basics of user experience design, discovered the importance of UX research, and brought ideas to life using Figma. By the end, many had created their first UI screens—walking away with both inspiration and practical skills.",
-		image: {src: uiuxEssentials, alt: "UI/UX Essentials Workshop"},
+		image: {src: "https://res.cloudinary.com/ieeescusb/image/upload/v1763109240/uiux_essentials_mnremt.webp", alt: "UI/UX Essentials Workshop"},
 		socialMediaPostLinks: {
 			facebook: "https://www.facebook.com/share/p/18WKwyG9jn/",
 			instagram: "https://www.instagram.com/p/C_8oYSbssH4/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
@@ -114,7 +98,7 @@ export const latestEvents: LatestEvent[] = [
 		location: "Creativa Hub, Ismailia",
 		description:
 			"Landing your dream job takes more than just a good degree. In this session, participants honed essential career-launching skills—from writing standout CVs and cover letters to acing interviews and optimizing LinkedIn profiles. Soft skills and strategic thinking were front and center, giving attendees a solid foundation for career growth.",
-		image: {src: hrGuided, alt: "HR Guided Workshop"},
+		image: {src: "https://res.cloudinary.com/ieeescusb/image/upload/v1763109236/hr_guided_ph3loo.webp", alt: "HR Guided Workshop"},
 		socialMediaPostLinks: {
 			facebook: "https://www.facebook.com/share/p/188UCBAs4e/",
 			instagram: "https://www.instagram.com/p/C_0y9iPsEiM/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
@@ -129,7 +113,7 @@ export const latestEvents: LatestEvent[] = [
 		location: "Creativa Hub, Ismailia",
 		description:
 			"Creating engaging content takes more than just hitting “record.” This session took participants through the essentials of video editing, focusing on how to turn raw footage into polished Reels and Shorts. From trimming clips to adding professional flair, attendees learned how to make videos that truly stand out.",
-		image: {src: makeYourReel, alt: "Make Your Reel Workshop"},
+		image: {src: "https://res.cloudinary.com/ieeescusb/image/upload/v1763109236/make_your_reel_w7bwin.webp", alt: "Make Your Reel Workshop"},
 		socialMediaPostLinks: {
 			facebook: "https://www.facebook.com/share/p/1B8paCCeRP/",
 			linkedin:
