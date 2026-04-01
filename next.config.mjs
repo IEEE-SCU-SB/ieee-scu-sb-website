5d73292b6049149b710e570fc00031b29a06a360
saeedinum <mo7amed0ptr@gmail.com>
2026-04-01 16:42:42 +0200

    refactor: Update project configuration and structure

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
 	images: {
 		formats: ["image/webp", "image/avif"],
+		qualities: [100, 75],
 		minimumCacheTTL: 60,
 		deviceSizes: [128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
 		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
